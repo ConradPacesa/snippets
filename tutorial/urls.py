c7975e37675d5a9bfb1572cf53fe8f19c3e64295
Conrad Pacesa <conradpacesa@gmail.com>
2017-08-30 00:02:58 -0400

    Created hyperlinked relationships

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.conf.urls import include
 
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     url(r'^', include('snippets.urls')),
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
